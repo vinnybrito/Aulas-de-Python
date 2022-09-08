4468fbd7c38c03d22f1cd35ebc1524f5bf773c77
vinnybrito <viny.rbrito15@hotmail.com>
2022-09-08 07:27:17 -0300

    news ex

--- Exercicios-Python1/Ex39-repetiçãoF.py
+++ Exercicios-Python1/Ex39-repetiçãoF.py
@@ -1,6 +1,6 @@
 
 v = 30
-n1 = 0
+n1 = 1
 n2 = 1
 n3 = 3
 
@@ -9,7 +9,7 @@ print(f'2 : {n2}')
 
 for n3 in range(1, 31):
     a = n1 + n2
-    print(f'{n3} : {a}')
+    print(f'{n2} : {a}')
     n1 = n2
     n2 = a
     n3 = n3 + 1
